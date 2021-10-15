a1c42d304528f36b4fbe4716904584317637351b
Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-10-15 14:20:14 +0200

    Creacion de index.php y configdb.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="author" content="rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net" />
+        <title>BDEmpleados</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
