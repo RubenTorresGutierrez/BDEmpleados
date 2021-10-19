ca5e093bd1c8595b03857940269e60f339bc850a
Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-10-19 13:28:51 +0200

    Se añade el método anadirEmpleado, hay un error

--- empleado.php
+++ empleado.php
@@ -1,16 +1,37 @@
 <?php
 
+    //IMPORTACIONES
+    require_once 'configdb.php';
+
     class Empleado{
+
+        //OBJETOS
+        // Se instancia un objeto desde la clase mysqli con los datos 
+        // de conexión importados en forma de constantes desde configdb.php
+        $mysqli = new mysqli(URL, USUARIO, PASSWD, DATABASE);
+
+        //ATRIBUTOS
         private $dni;
         private $nombre;
         private $correo;
         private $telefono;
 
-        function __construct($dni, $nombre, $correo, $telefono){
+        function __construct($dni, $nombre, $correo = '', $telefono){
             $this->dni = $dni;
             $this->nombre = $nombre;
             $this->correo = $correo;
             $this->telefono = $telefono;
+            $this->anadirEmpleado();
+        }
+        function anadirEmpleado(){
+            //Consulta SQL para añadir una fila nueva a la tabla empleado
+            $sql =  'INSERT INTO empleado(dni, nombre, correo, telefono)'.
+                    "VALUES('".$this->dni."', '".$this->nombre."', '".$this->correo."', '".$this->telefono."');"
+            //Mandar la consulta a la Base de Datos
+            $mysqli->query($sql);
+        }
+        function mostrarEmpleado(){
+
         }
     }
 
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
     //IMPORTACIONES
-    require_once 'configdb.php';
+    require_once 'empleado.php';
 
-    //OBJETOS
-    // Se instancia un objeto desde la clase mysqli con los datos 
-    // de conexión importados en forma de constantes desde configdb.php
-    $mysqli = new mysqli(URL, USUARIO, PASSWD, DATABASE);
+    if (isset($_POST['enviar'])){
+        if (!empty($_POST['dni']) && !empty($_POST['nombre']) && !empty($_POST['telf'])) {
+            $empleado = new Empleado($_POST['dni'], $_POST['nombre'], $_POST['correo']), $_POST['telf']));
+            $empleado->anadirEmpleado();
+        }
+    }
 
 ?>
 
@@ -40,22 +42,22 @@
 </html>
     <?php
 
-        if (isset($_POST['enviar'])){
-            if (!empty($_POST['nombre'])) {
-                $sql = "SELECT * FROM alumno WHERE Nombre LIKE '%" . implode(' ', array_filter(explode(' ', $_POST['nombre']))) . "%';";
-                $resultado = $mysqli -> query($sql);
-                if ($resultado -> num_rows > 0) {
-                    while ($fila = $resultado -> fetch_array()) {
-                        echo $fila['NumAlumno'] . ': ';
-                        echo $fila['Nombre'] . ' | ';
-                        if ($fila['Repite'] == 0)
-                            echo 'El alumno NO repite.';
-                        else echo 'El alumno repite';
-                        echo '<br />';
-                    }
-                }else echo 'No se han encontrado resultados';
-            }else echo 'El campo está vacío.';
-        }
-        $mysqli -> close();
+        // if (isset($_POST['enviar'])){
+        //     if (!empty($_POST['nombre'])) {
+        //         $sql = "SELECT * FROM alumno WHERE Nombre LIKE '%" . implode(' ', array_filter(explode(' ', $_POST['nombre']))) . "%';";
+        //         $resultado = $mysqli -> query($sql);
+        //         if ($resultado -> num_rows > 0) {
+        //             while ($fila = $resultado -> fetch_array()) {
+        //                 echo $fila['NumAlumno'] . ': ';
+        //                 echo $fila['Nombre'] . ' | ';
+        //                 if ($fila['Repite'] == 0)
+        //                     echo 'El alumno NO repite.';
+        //                 else echo 'El alumno repite';
+        //                 echo '<br />';
+        //             }
+        //         }else echo 'No se han encontrado resultados';
+        //     }else echo 'El campo está vacío.';
+        // }
+        // $mysqli -> close();
 
     ?>
