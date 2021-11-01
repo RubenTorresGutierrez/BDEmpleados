2db302077aaae38a9c2434c7837ca08cb22ae7f3
RubenTorres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-11-01 20:16:52 +0100

    index y style pasados %, faltan bordes e iconos

--- elementoshtml.php
+++ elementoshtml.php
@@ -5,6 +5,7 @@
         echo '<head>'.
                 '<meta charset="utf-8" />'.
                 '<meta name="author" content="rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net" />'.
+                '<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">'.
                 '<title>'.$titulo.'</title>'.
                 '<link rel="stylesheet" href="css/style.css" type="text/css" />'.
                 '<link rel="stylesheet" href="css/'.$css.'.css" type="text/css" />'.
